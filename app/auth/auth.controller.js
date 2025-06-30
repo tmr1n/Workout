264088c03cd97a095d8b36d1355297740812bf03
tmr1n <163327345+tmr1n@users.noreply.github.com>
2025-06-30 20:51:04 +0200

    Add JWT

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,16 +1,21 @@
 //@desc Authenticate user
 //@route POST /api/auth/user
 //@access Public
+import { faker } from '@faker-js/faker'
+import { hash } from 'bcryptjs'
 import asyncHandler from 'express-async-handler'
+
 import { prisma } from '../prisma.js'
+
+import { generateToken } from './generate-token.js'
 
 // @desc Authenticate user
 // @route POST /api/auth/user
 // @access Public
-export const authUser = asyncHandler (async (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
 	const user = await prisma.user.findMany({
 		where: {
-			password1:'werf'
+			password1: 'werf'
 		}
 	})
 	res.json(user)
@@ -20,29 +25,35 @@ export const authUser = asyncHandler (async (req, res) => {
 // @route POST /api/auth/user
 // @access Public
 
-export const registerUser = asyncHandler (async (req, res) => {
+export const registerUser = asyncHandler(async (req, res) => {
 	const { email, password } = req.body
 
+	// Проверка, существует ли пользователь по email
 	const isHaveUser = await prisma.user.findUnique({
-		where: {
-			email, password: await hash(password), name: faker.name.fullName()
-		}
+		where: { email }
 	})
-
-	const token = generateToken()
 
 	if (isHaveUser) {
 		res.status(400)
 		throw new Error('User already exists')
 	}
 
+	// Хэшируем пароль
+	const hashedPassword = await hash(password, 10)
+
+	// Создаём пользователя
 	const user = await prisma.user.create({
 		data: {
 			email,
-			password: password
+			password: hashedPassword,
+			name: faker.name.fullName()
 		}
 	})
 
-	res.json(req.body)
+	// Генерация токена после создания пользователя
+	const token = generateToken(user.id)
+
+	res.json({ user, token })
 })
+
 //http://localhost:5000/api/auth/register
